2037f25580073f1c0a3b4d7d6acad291d685dd99
SafaeQ <safaedani009@gmail.com>
2022-01-05 15:58:02 +0100

    test of the query is done

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-
+const {
+    QueryTypes
+} = require('sequelize');
 const db = new Sequelize('usermangementsystem', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
@@ -10,7 +12,18 @@ db.authenticate().then(() => {
 }).catch((error) => {
     console.error('cannot to connect to the database:', error)
 })
+
+
 // console.log('somthing..')
+// db.query('SELECT * FROM users')
+async function omma() {
+    const users = await db.query("SELECT * FROM `users`", {
+        type: QueryTypes.SELECT
+    });
+    console.log(users);
+}
+
+omma()
 
 module.exports = {
     db
