769133136681006652c660cd01eed8392edb8742
SafaeQ <safaedani009@gmail.com>
2022-01-05 11:47:32 +0100

    create connection

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
 const Sequelize = require('sequelize');
+const db = new Sequelize('usermangementsystem', 'root', '', {
+    host: 'localhost',
+    dialect: 'MySQL2'
+})
+try {
+
+} catch (error) {
+    console.error('cannot to connect to the database:', error)
+}
+
+module.exports = db;
