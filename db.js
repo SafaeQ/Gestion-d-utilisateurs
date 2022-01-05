87a2377219cab653beae25cc861d73d6f61b46ff
SafaeQ <safaedani009@gmail.com>
2022-01-05 14:20:00 +0100

    edit db.js

--- db.js
+++ db.js
@@ -3,7 +3,18 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('usermangementsystem', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-
+    logging: function () {},
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    dialectOptions: {
+        socketPath: "/var/run/mysqld/mysqld.sock"
+    },
+    define: {
+        paranoid: true
+    }
 })
 
 db.authenticate().then(() => {
