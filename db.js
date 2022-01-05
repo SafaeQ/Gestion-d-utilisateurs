f037139e2ffc1f3f7accad620f9baf3501dc6213
SafaeQ <safaedani009@gmail.com>
2022-01-05 14:16:51 +0100

    delete file

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('usermangementsystem', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+
 })
 
 db.authenticate().then(() => {
