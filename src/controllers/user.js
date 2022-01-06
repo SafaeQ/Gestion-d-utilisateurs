eb2f5addbac175954c4df00afb38e60097ecca9f
SafaeQ <safaedani009@gmail.com>
2022-01-06 11:46:48 +0100

    create code userController

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,17 @@
-const userController = (req, res) => {
-    res.render('user', {
+const User = require('../models/Users')
 
-    })
+
+function getAllUsers(req, res, next) {
+    return User
+        .findAll({
+            paranoid: false
+        })
+        .then(users => res.status(200).json({
+            status: 'okkk',
+            users
+        }))
+        .catch(err => console.log(err));
 }
-
 module.exports = {
-    userController,
+    getAllUsers,
 }
