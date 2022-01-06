3f198d9bdac43ecb668013d2d612759e47127760
SafaeQ <safaedani009@gmail.com>
2022-01-06 17:02:07 +0100

    update

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
-const sequelize = require('./db')
-const {
-    getAllUsers
-} = require('./src/controllers/user')
+// const sequelize = require('./db')
+const db = require('./models')
+// const {
+//     getAllUsers
+// } = require('./src/controllers/user')
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs')
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs')
 
-const port = 9000
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
-app.get('/all', (req, res) => {
-    getAllUsers(res, req).then(users => res.send(users))
-})
-app.listen(port, () => {
-    console.log(`listening on port  http://localhost:${port}`);
+// const port = 9000
+// app.get('/', (req, res) => {
+//     res.send('hello world');
+// })
+// app.get('/all', (req, res) => {
+//     getAllUsers(res, req).then(users => res.send(users))
+// })
+db.sequelize.sync().then((req) => {
+    app.listen(2000, () => {
+        console.log(`listening on port  `);
+    })
 })
