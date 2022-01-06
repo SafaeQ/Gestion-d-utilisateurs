5e8d3069d926addf5919f710d1f58008fad56d95
SafaeQ <safaedani009@gmail.com>
2022-01-06 17:17:40 +0100

    create server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const db = require('./models')
+// const {
+//     getAllUsers
+// } = require('./src/controllers/user')
+
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs')
+
+const port = 9000
+// app.get('/', (req, res) => {
+//     res.send('hello world');
+// })
+// app.get('/all', (req, res) => {
+//     getAllUsers(res, req).then(users => res.send(users))
+// })
+db.sequelize.sync().then((req) => {
+    app.listen(2000, () => {
+        console.log(`listening on port ${port}`);
+    })
+})
