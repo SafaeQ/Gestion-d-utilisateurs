61a4f70855f666e11123b9ff58271d214b7912b7
SafaeQ <safaedani009@gmail.com>
2022-01-06 17:18:32 +0100

    create userModel

---
+++ models/User.js
@@ -0,0 +1,35 @@
+// 'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Utilisateur = sequelize.define('Utilisateur', {
+        nomComplet: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+    });
+    // Utilisateur.associate = function (models) {
+    //     Utilisateur.belongsTo(models.Departement, {
+    //         as: 'departement',
+    //         foreignKey: 'idDepartement'
+    //     });
+    // };
+    return Utilisateur;
+};
+
+
+// const models = require('../models/Users')
+
+
+// controllers
+// function getAllUsers(req, res, next) {
+//     return models.User
+//         .findAll({
+//             paranoid: false
+//         })
+//         .then(users => res.status(200).json({
+//             status: 'okkk',
+//             users
+//         }))
+//         .catch(err => console.log(err));
+// }
+// module.exports = {
+//     getAllUsers,
+// }
