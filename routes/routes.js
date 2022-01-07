24968b16e04b95115d02a2419deac6a32c41ffb1
SafaeQ <safaedani009@gmail.com>
2022-01-07 15:44:31 +0100

    cmnt

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,9 @@ const {
     getAllUsers
 } = require('../controllers/userController')
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+// router.get('/', (req, res) => {
+//     res.render('home');
+// })
 router.get('/all', getAllUsers)
 router.get('/create', )
 module.exports = router;
