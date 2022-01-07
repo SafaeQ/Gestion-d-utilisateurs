3b878ab0915f5d96a58a1690181459df968eae1e
SafaeQ <safaedani009@gmail.com>
2022-01-07 16:15:55 +0100

    create USer

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,26 @@ const oneUser = async (req, res) => {
     }).catch(err => console.log(err))
     res.send(user)
 }
+
+const createUser = async (req, res) => {
+    const {
+        account_name,
+        email,
+        password,
+        id_ped
+    } = await req.body;
+    const user = await User.create({
+        account_name,
+        email,
+        password,
+        id_ped
+    }).catch(err => console.log(err))
+    console.log(user)
+    res.send(user)
+    // await res.redirect('/')
+}
 module.exports = {
     getAllUsers,
     oneUser,
-    // createUser,
+    createUser,
 }
