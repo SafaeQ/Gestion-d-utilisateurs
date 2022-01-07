8f3e3951710825be1db974c83f5a270d866159ae
SafaeQ <safaedani009@gmail.com>
2022-01-07 14:42:52 +0100

    delete line

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html')
     res.render('home');
 })
 module.exports = router;
