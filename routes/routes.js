598675db08562d792cf80dd5e4da0ce4e7769a48
SafaeQ <safaedani009@gmail.com>
2022-01-07 15:41:46 +0100

    create router

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
     getAllUsers
 } = require('../controllers/userController')
+
 router.get('/', (req, res) => {
     res.render('home');
 })
 router.get('/all', getAllUsers)
+router.get('/create', )
 module.exports = router;
