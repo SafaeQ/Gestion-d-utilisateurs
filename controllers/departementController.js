85370aad903ea6b81bac49724a28d106cace5cd6
SafaeQ <safaedani009@gmail.com>
2022-01-08 21:39:56 +0100

    create function getAllDepartement

--- controllers/departementController.js
+++ controllers/departementController.js
@@ -1,3 +1,14 @@
 const {
     Departement
-} = require('../models/DepartementModeljs')
+} = require('../models/DepartementModel')
+
+const getAllDepartement = async (req, res) => {
+    const departements = await Departement.findAll({
+        raw: req.query.raw,
+    }).catch(err => err.message)
+    res.send(departements)
+}
+
+module.exports = {
+    getAllDepartement
+}
