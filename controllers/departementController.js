882f060020c56bb7d3a6b11873b0f3eb4272dbe1
SafaeQ <safaedani009@gmail.com>
2022-01-10 11:53:24 +0100

    ad update function of depatrtement

--- controllers/departementController.js
+++ controllers/departementController.js
@@ -4,13 +4,49 @@ const {
 
 const getAllDepartement = async (req, res) => {
     const departements = await Departement.findAll({
-        raw: req.query.raw,
+        raw: true
     }).catch(err => err.message)
     res.send(departements)
+}
+
+const getDepartement = async (req, res) => {
+    const departement = await Departement.findOne({
+        raw: true
+    }).catch(err => err.message)
+    res.send(departement)
+}
+
+const updateDepartement = async (req, res) => {
+    const {
+        id
+    } = req.params.id;
+    const departement = await Departement.update({
+            where: {
+                id: id
+            }
+        }).then(x => {
+            if (x == 1) {
+                res.send({
+                    message: 'Departement updated'
+                })
+            } else {
+                res.send({
+                    message: 'cannot updated'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
+    res.send(departement)
 }
 
 
 module.exports = {
     getAllDepartement,
+    getDepartement,
+    updateDepartement,
 
 }
