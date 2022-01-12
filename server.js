6f179954f9aee650993aefe5e935165d89644a21
SafaeQ <safaedani009@gmail.com>
2022-01-12 19:30:45 +0100

    server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const router = require('./routes/routes');
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: true
+// }));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
