a9a2702540bec481f69bd6ff95052e42c83f6b87
SafaeQ <safaedani009@gmail.com>
2022-01-14 21:14:23 +0100

    .

--- views/dom.js
+++ views/dom.js
@@ -1,6 +1,7 @@
-let x = doocument.querySelectorAll('.edit')
-document.querySelectorAll('.edit').forEach(edit => edit.addEventListener('click', () => {
+let x = doocument.querySelectorAll('.edit');
+x.forEach(edit => edit.addEventListener('click', () => {
     console.log(edit.parentElement.parentElement.children[1])
+
 }))
 
 document.body.parentElement
