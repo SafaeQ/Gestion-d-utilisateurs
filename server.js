0d12aed856b4f493c5603f729194d74937a52d6c
SafaeQ <safaedani009@gmail.com>
2022-01-14 13:11:40 +0100

    good

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+// static files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'assets/css'))
+// set template engine
+app.use(expressLayouts)
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 app.use('/', router);
 
 app.listen(port, () => {
