227a487eed1fa4fc495c4853e38046b8b254c685
SafaeQ <safaedani009@gmail.com>
2022-01-14 21:16:09 +0100

    delete file dom

--- views/dom.js
+++
@@ -1,7 +0,0 @@
-let x = doocument.querySelectorAll('.edit');
-x.forEach(edit => edit.addEventListener('click', () => {
-    console.log(edit.parentElement.parentElement.children[1])
-
-}))
-
-document.body.parentElement
