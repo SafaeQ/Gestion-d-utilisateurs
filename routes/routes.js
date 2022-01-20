ce59a9ffd99167889d0e704a3266529bc7048563
SafaeQ <safaedani009@gmail.com>
2022-01-20 21:40:24 +0100

    ok

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // create new obj of router
 
 // import functions of users from controller (CRUD)
 const {
